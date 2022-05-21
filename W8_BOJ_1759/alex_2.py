af242cd0dae3f70c006425d3e4a2a1b023b0c2bd
Alex Yang <alex@kidsnote.com>
2022-05-22 00:00:18 +0900

    [alex] add comment

--- W8_BOJ_1759/alex_2.py
+++ W8_BOJ_1759/alex_2.py
@@ -2,9 +2,10 @@
 l, c = map(int, input().split())
 j = ['a', 'e', 'i', 'o', 'u']
 cl = input().split()
-cl.sort()
+cl.sort() # 이 정렬 한방으로 정렬조건은 만족됨.
 
-
+# arr에서 length개의 원소를 뽑는 함수
+# length값을 줄여가며 재귀로 조합을 구한다.
 def comb(arr, length):
     res = []
     if length > len(arr):
@@ -18,11 +19,14 @@ def comb(arr, length):
                 res.append([arr[i]] + j)
     return res
 
+# 조합 목록을 구해서 loop
 for s in comb(cl, l):
     cnt = 0
+    # 자음 카운트
     for a in j:
         if a in s:
             cnt += 1
 
+    # 조건에 맞으면 출력
     if cnt and l - cnt >= 2:
         print(''.join(s))
