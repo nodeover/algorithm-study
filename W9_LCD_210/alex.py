f8fb51c19ac78c164b44885abd488e65696eb9f0
Alex Yang <airtag@outlook.com>
2022-06-08 20:16:44 +0900

    아직 코드 미완성

--- W9_LCD_210/alex.py
+++ W9_LCD_210/alex.py
@@ -1,41 +1,24 @@
-from collections import deque
-from typing import List
+from collections import defaultdict
 
+while True:
+    m = int(input())
+    if m == 0:
+        break
+    s = input()
+    hd = defaultdict(int)
 
-class Solution:
-    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
-        ds = [0] * numCourses
-        q = deque()
-        ans = []
+    left, right = -1, -1
+    max_cnt = 0
+    while left <= right and right < len(s) - 1:
+        cnt = len(hd)
+        if cnt < m or (cnt == m and hd[s[right + 1]] > 0):
+            right += 1
+            hd[s[right]] += 1
+        else:
+            left += 1
+            hd[s[left]] -= 1
+            if hd[s[left]] == 0:
+                del hd[s[left]]
+        max_cnt = max(right - left, max_cnt)
 
-        # 차수 구하기
-        for next, prev in prerequisites:
-            ds[next] += 1
-
-        # 각 정점 번호에서 0차수인 것들 큐에 추가
-        for i in range(numCourses):
-            if ds[i] == 0:
-                q.append(i)
-
-        # 각 정점 번호별
-        for i in range(numCourses):
-            # q가 비어버리면 [] 출력
-            if not q:
-                return []
-            # q에 있는 내용 결과에 추가
-            ans.append(q.popleft())
-
-            # 정점 탐색
-            for next, prev in prerequisites:
-                # 마지막 결과의 정점이랑 정점의 출발점과 같을 경우 차수를 빼준다.
-                if ans[-1] == prev:
-                    ds[next] -= 1
-                    # 정점이 0이 되었으면 큐에 추가
-                    if ds[next] == 0:
-                        q.append(next)
-        return ans
-
-
-s = Solution()
-print(s.findOrder(2, [[1, 0]]))
-print(s.findOrder(4, [[1, 0], [2, 0], [3, 1], [3, 2]]))
+    print(max_cnt)
