946d469689b94eb6f35f140190fcf4ff8c5a616f
Alex Yang <airtag@outlook.com>
2022-07-20 01:00:52 +0900

    [alex] 13주차 pass

--- W13_BOJ_7453/alex.py
+++ W13_BOJ_7453/alex.py
@@ -1,27 +1,40 @@
-from collections import defaultdict
+import sys
+
+input = sys.stdin.readline
 
 n = int(input())
-A, B, C, D = [], [], [], []
+ab, cd = [], []
 
-for _ in range(n):
-    a, b, c, d = map(int, input().split())
-    A.append(a)
-    B.append(b)
-    C.append(c)
-    D.append(d)
+l = [list(map(int, input().split())) for _ in range(n)]
+for i in range(n):
+    for j in range(n):
+        ab.append(l[i][0] + l[j][1])
+        cd.append(l[i][2] + l[j][3])
 
-pair1 = defaultdict(int)
-pair2 = defaultdict(int)
-for a in A:
-    for b in B:
-        pair1[a + b] += 1
-
-for c in C:
-    for d in D:
-        pair2[c + d] += 1
+ab.sort()
+cd.sort()
 
 ans = 0
-for pair in pair1.keys():
-    if pair * -1 in pair2.keys():
-        ans += pair1[pair]
+p_ab, p_cd = 0, len(cd) - 1
+
+while p_ab < len(cd) and p_cd >= 0:
+    sum_val = ab[p_ab] + cd[p_cd]
+    if sum_val == 0:
+        f_ab, f_cd = 1, 1
+
+        while p_ab + f_ab < len(cd) and ab[p_ab] == ab[p_ab + f_ab]:
+            f_ab += 1
+
+        while p_cd - f_cd >= 0 and cd[p_cd] == cd[p_cd - f_cd]:
+            f_cd += 1
+
+        ans += f_ab * f_cd
+        p_ab += f_ab
+        p_cd -= f_cd
+
+    elif sum_val > 0:
+        p_cd -= 1
+    else:
+        p_ab += 1
+
 print(ans)
